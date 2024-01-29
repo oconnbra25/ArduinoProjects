bf3011676c940368823afc12d52e0a08d4286579
oconnbra25 <123963813+oconnbra25@users.noreply.github.com>
2024-01-29 10:29:21 -0500

    Update Lab1.ino

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,32 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
-pinMode(13,OUTPUT);
-pinMode(11,OUTPUT);
-pinMode(9,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(3,OUTPUT);
+// Brady O'Connor
+// Create "traffic light"
+// Start red on for 5 sec
+// Green for 5 sec
+// Yellow for 2 sec
+// Back to red
+
+void setup()
+{
+  pinMode(13, OUTPUT); // Red
+  pinMode(12, OUTPUT); // Yellow
+  pinMode(11, OUTPUT); // Green
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(13,HIGH); //light 1 green
-digitalWrite(3,HIGH); //light 2 red
-delay(5000);          //wait5 sec
-digitalWrite(13,LOW); //light 1 turns yellow
-digitalWrite(11,HIGH);
-delay(2000);
-digitalWrite(11,LOW);
-digitalWrite(9,HIGH); //light 1 red
-digitalWrite(3,LOW);
-digitalWrite(7,HIGH); //light 2 green
-delay(5000);
-digitalWrite(7,LOW); //light 2 yellow
-digitalWrite(5,HIGH);
-delay(2000);
-digitalWrite(5,LOW); //end of light 2 yellow
-
-//this code is broken for some reason, could have done something wrong with arduiino itself
-//attempt to run 2 traffic lights at the same time
-
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(5000); // Wait for 5 seconds
+  digitalWrite(13, LOW);
+  digitalWrite(11, HIGH);
+  delay(5000); // Wait for 5 seconds
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay(2000); // Wait for 2 seconds
+  digitalWrite(12, LOW);
 }
